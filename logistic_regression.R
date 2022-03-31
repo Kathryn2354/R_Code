f9bf2ff5d512b0da0427e286d93aca0d6f0955c8
Kathryn Li <96843197+Kathryn2354@users.noreply.github.com>
2022-03-30 21:33:18 -0400

    Add files via upload

---
+++ logistic_regression.R
@@ -0,0 +1,17 @@
+library(ISLR)
+library(dplyr)
+library(ggplot2)
+data3 <- Hitters 
+happy3 <- sample(2, nrow(data), replace = T, prob = c(0.8, 0.2)) 
+train3 <- data.frame(data[happy==1, ])
+test3 <- data.frame(data[happy==2, ])
+
+model3 <- glm(AtBat ~ Hits,  family = "binomial", data = train)
+
+Predicted_data3 <- data.frame(Hits=seq(min(data$Hits), max(data$Hits), len=500))
+
+Predicted_data3$AtBat <- predict(model, Predicted_data, type = "response")
+
+ggplot(train, aes(x=Hits, y=AtBat)) + geom_point() + stat_smooth(method ="glm", color = "green", se=FALSE, method.args = list(family = binomial)) +geom_smooth()
+
+                                                                 
